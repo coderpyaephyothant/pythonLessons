0e3a240ad82acb12222b3343efc3b727489f39ba
coderpyaephyothant <pyaephyothantengr520@gmail.com>
2024-01-03 22:45:55 +0630

    pythonLesson

---
+++ lesson3.py
@@ -0,0 +1,40 @@
+# variable
+x = 2 
+y = 3.3
+z = "green"
+
+x = y = z = 20
+print("x",x)
+print("y",y)
+print("z",z)
+x = 5
+y = 15
+
+print("x",x)
+print("y",y)
+
+# identifier
+
+# def function ():
+
+# class pyae:
+
+# letters digits underscores
+
+pyae = 27
+p1 = 27
+p1_p2 = 27
+_p3 = 27
+
+# Python Datatypes
+
+# numbers
+# integer/float/complex
+
+
+# strings
+# List
+# Tuple
+# dictionary
+
+
