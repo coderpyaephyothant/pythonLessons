d251adf5808b489f648f2f4e1b280907a9a6cfe9
coderpyaephyothant <pyaephyothantengr520@gmail.com>
2024-01-28 10:39:50 +0630

    lesson10lotterygameerrFix

--- 10lesson.py
+++ 10lesson.py
@@ -6,7 +6,6 @@ secureNumber = secrets.SystemRandom()
 
 while True:
     print('_________Welcome to 2D game with Python Programming_________')
-    # exit()
     press1 = int(input('______Press 1 for RULES OR 2 to start Game!____'))
     if press1 == 1:
         print(' >Your Age must be greater than or equal 17! ')
@@ -18,54 +17,69 @@ while True:
         print('Lets Start!')
         clientName = input('Type your name : at least 3 words')
         clientAge = int(input('Type your age : at least 17year!'))
+        print('_____you entered___', len(clientName))
+        print('_____you entered___', clientAge)
         if len(clientName) < 3:
             print('Your Name must be aleast 3Characters!')
+            exit()
         if clientAge < 17:
             print('Your Age must be aleast 17!')
             exit()
-        if len(clientName) > 3 and clientAge >= 17:
+        while len(clientName) >= 3 and clientAge >= 17:
             print('You can play game Now. ...')
             print('Welcome', clientName ,'!. . ')
-            clientMoney = int(input('Now, Enter your coin! : aleast 10000 Coin'))
-            while clientMoney < 10000 or None:
-                print('Coin must be aleast 10000!', clientName, 'coin is', clientMoney )
-                clientMoney =  int(input('Please valid coin!'))
-                if clientMoney < 10000:
-                    continue
-            if clientMoney >= 10000:
-                print('Hello', clientName, 'Thanks You! - - - -')
-                clientUseMoney = int(input('How many conin wanna use ? Enter. :aleast 1000 : '))
-                while clientUseMoney < 1000 or None:
-                    print('Use Coin must be aleast 1000!', clientName, 'Used coin is : ', clientUseMoney )
-                    clientUseMoney =  int(input('Please valid Use coin!'))
-                    if clientUseMoney < 1000:
-                        continue   
-                if clientUseMoney >= 1000:
-                    print('You used ', clientUseMoney , 'coin for game!- - -')
-                    luckyNum = int(input('Insert your lucky Number! Must be two Number!------ '))
-                    while len(str(luckyNum)) < 2 or None:
-                        print('Use two number!', clientName, 'inserted lucky number is', luckyNum )
-                        luckyNum =  int(input('Please valid Luck Number!'))
-                        while len(str(luckyNum)) < 2 or len(str(luckyNum)) > 2:
-                            continue 
-                    if len(str(luckyNum)) == 2:
-                        sysNumber = secureNumber.randint(10,99)
-                        countNum = 0
-                        while countNum < 3:
-                            print('Wait... What is Number Today is . . .')
-                            countNum +=1
-                        if sysNumber == luckyNum:
-                            clientMoney = clientMoney + clientUseMoney
-                            print('Today Number is : ', sysNumber)  
-                            print('Hey',clientName,'Congratulation. . .You Won!!!!----')
-                            print('Hey',clientName,'You got',clientMoney)
-                        else:
-                            clientMoney = clientMoney - clientUseMoney
-                            print("Today number is :", sysNumber)
-                            print("Your Lucky number is :", luckyNum)
-                            print("Sorry..Good Luck again", clientName)
-                            print('Hey',clientName,'Your Coin : ',clientMoney)
-    print('Your Coin',clientMoney)
+            while True:
+                clientMoney = int(input('Now, Enter your coin! : aleast 10000 Coin'))
+                while clientMoney < 10000 or None:
+                    print('Coin must be aleast 10000!', clientName, 'coin is', clientMoney )
+                    clientMoney =  int(input('Please valid coin!'))
+                    if clientMoney < 10000:
+                        continue
+                while clientMoney >= 10000:
+                    if clientMoney >= 10000:
+                        print('your money is ', clientMoney)
+                        print('Hello', clientName, 'Thanks You! - - - -')
+                        clientUseMoney = int(input('How many conin wanna use ? Enter. :aleast 1000 : '))
+                        while clientUseMoney < 1000 or None:
+                            print('Use Coin must be aleast 1000!', clientName, 'Used coin is : ', clientUseMoney )
+                            clientUseMoney =  int(input('Please valid Use coin!'))
+                            if clientUseMoney < 1000:
+                                continue   
+                        if clientUseMoney >= 1000:
+                            print('You used ', clientUseMoney , 'coin for game!- - -')
+                            luckyNum = int(input('Insert your lucky Number! Must be two Number!------ '))
+                            while len(str(luckyNum)) < 2 or None:
+                                print('Use two number!', clientName, 'inserted lucky number is', luckyNum )
+                                luckyNum =  int(input('Please valid Luck Number!'))
+                                while len(str(luckyNum)) < 2 or len(str(luckyNum)) > 2:
+                                    continue 
+                            if len(str(luckyNum)) == 2:
+                                sysNumber = secureNumber.randint(10,99)
+                                countNum = 0
+                                while countNum < 3:
+                                    print('Wait... What is Number Today is . . .')
+                                    countNum +=1
+                                if  luckyNum == sysNumber:
+                                    clientMoney = clientMoney + clientUseMoney
+                                    print('Today Number is : ', sysNumber)  
+                                    print('Hey',clientName,'Congratulation. . .You Won!!!!----')
+                                    print('Hey',clientName,'You got',clientMoney)
+                                
+                                    playAgainNum = int(input('enter 1  to play again OR Enter 0 to QUIT****'))
+                                    while clientMoney > 0:
+                                        if playAgainNum == 1:
+                                            break
+                                    while playAgainNum == 0:
+                                        exit()
+                                else:
+                                    clientMoney = clientMoney - clientUseMoney
+                                    print("Today number is :", sysNumber)
+                                    print("Your Lucky number is :", luckyNum)
+                                    print("Sorry..Good Luck again", clientName)
+                                    print('Hey',clientName,'Your Coin : ',clientMoney)
+                                if clientMoney == 0 or clientMoney < 1000:
+                                    exit()
+
                         
                         
 
