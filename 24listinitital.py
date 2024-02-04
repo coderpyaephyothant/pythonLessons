bfdb7a5c62e76dc93a740e4f9f256de9c2fe1942
coderpyaephyothant <pyaephyothantengr520@gmail.com>
2024-02-04 22:57:08 +0630

    24lesson

---
+++ 24listinitital.py
@@ -0,0 +1,10 @@
+# List Initial
+pptList = [20,30,40,50]
+# print(pptList[3])
+# print(pptList[-1],'reverse')
+
+# slicing
+# print(pptList[:])
+# print(pptList[0:2])
+# print(pptList[0:-1])
+# print(pptList[0:])
