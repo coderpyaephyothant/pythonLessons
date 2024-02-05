769048fd35e6b47487c69efb4af1c2db87e6ba95
coderpyaephyothant <pyaephyothantengr520@gmail.com>
2024-02-06 00:19:39 +0630

    252627

---
+++ 25lesson.py
@@ -0,0 +1,19 @@
+pptNames = ['pyae','phyo','thant']
+# add new data
+pptNames.append('Chinese')
+# print('pptNames Updated',pptNames)
+
+# rempve() 
+# toRemoveName = pptNames[3]
+# toRemoveName = pptNames[-1]
+# print('toRemoveName',toRemoveName)
+# pptNames.remove(toRemoveName)
+# print('removed a name',pptNames)
+
+# pop()
+# pptNames.pop(3)
+# print(pptNames)
+
+# del keyword
+# del pptNames[3]
+# print('used del keyword',pptNames)
